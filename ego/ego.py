c942ef3cf3f00ba92e660bc36f148ba837a7804f
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2023-07-11 16:17:54 +0800

    Update ego.py

--- ego/ego.py
+++ ego/ego.py
@@ -32,9 +32,11 @@ E = readCsv("ego/nlp_output/entityDict.csv")
 # DocDTM = readCsv("nlp_output/DocDTM.csv")
 
 
-
+# class_list is a list save all type of node's class in our dataset
 class_list=["公司", "國家（地區）", "組織", "火箭", "衛星", "術語"]
+# E_list is a English version class_list
 E_list = ["com","loc","org","rocket","satellite","term"]
+# each colour in colour list represent a specific class in class_list
 colour = ["#FFFAA0", "#A3B7F9", "#C1B5C5", "#FFB86B", "#ff6961", "#a6cba3"]
 
 # add an Mandarin label in dictionary
@@ -105,7 +107,7 @@ with st.sidebar:
     # Dump Settings button has no function now, wheras it can show the link which including specific variables
     st.button('Dump Settings')
 
-# 取出某個詞類的所有關鍵字，並取得其dtm矩陣
+# extract keywords form the specific class then get it Document Term Matrix (DTM)
 if Unit == '篇':
     v = DocCR[Z].to_frame()
 else:
@@ -157,7 +159,8 @@ else:
         a = 'SenCO'
 
 x = pd.DataFrame(x)
-# there's no directionality between 'from' and 'to' remove the repeated data and the correlation coefficient corealated the node itself.
+# there's no directionality between 'from' and 'to' therefore we have to 
+# remove the repeated data and the correlation coefficient corealated the node itself.
 def matrix_to_xy(df, columns=None, reset_index=False):
     bool_index = np.triu(np.ones(df.shape), k=1).astype(bool)
     xy = (
@@ -170,6 +173,7 @@ def matrix_to_xy(df, columns=None, reset_index=False):
     return xy
 
 x = matrix_to_xy(x, reset_index=True)
+# only reserve the data which correlation coefficient is over 0
 x = x[x["val"] > 0]
 x = pd.DataFrame(x)
 # 'Q' is a relative value not absolute value
@@ -178,15 +182,22 @@ links = x[x["val"] >= round(float(x.quantile(Q).loc['val']),2)]
 # Initiate PyVis network object
 ego = Network(height='800px', bgcolor='#222222', font_color='white')
 
+# node_id: concatenate 'from' and 'to' then get rid of the repeated serial number in dataframe before transform its' type to list
 node_id = pd.unique(pd.concat([links['from'], links['to']])).tolist()
 node_label = []
 node_colour = []
 node_size = []
 node_title = []
 for i in node_id:
+    # node label is the keyword of node
     node_label.append(E.loc[i, 'keywords']) 
-    node_colour.append(E.loc[i, 'colour']) 
+    # node colour symbolize the class of the node 
+    node_colour.append(E.loc[i, 'colour'])
+    # node size presents how many times the keyword shows in articles
+    # The more time it is shown, the larger the node size is
     node_size.append(math.log(E.loc[i, 'freq'],2)+10)
+    # when the user clicks on a specific node, detailed information about that node will be shown
+    # the formate of the information is like: keyword(class, frequency)
     node_title.append(E.loc[i]['keywords']+ '('+ E.loc[i]['label']+ ', '+ str(E.loc[i]['freq'])+ ')')
 
 ego.add_nodes(node_id, 
@@ -201,14 +212,23 @@ for index, row in links.iterrows():
     ego.add_edge(row['from'], row['to'], value=row['val'])
 
 
+# use this path on your pc or your own notebook
+# path = os.getcwd().replace('\\','/') 
 
-# path = os.getcwd().replace('\\','/')
+# use this github path when app is connected to streamlit cloud 
 path = 'ego'
+
+# lengend_list is a list save lists which have a tuple and a string inside
 legend_list =[]
 for i in range(len(class_list)):
     legend=[]
+    # annotated_text format: ("keyword", "annotation", "high light colorr")
+    # legend's high light colour adding
     legend.append(("", "", colour[i]))
+    # add secific class after legend colour 
     legend.append(class_list[i])
+    # legend is like: [("", "", colour), class] 
+    # append these lengends into the legend_list
     legend_list.append(legend)
 
 
@@ -218,9 +238,13 @@ HtmlFile = open(f'{path}/node.html','r',encoding='utf-8')
 # seperate page into two part 8 for graph of nodes and 1 for lengends
 col1, col2= st.columns([8, 1])
 with col1:
+    # show the graph
     components.html(HtmlFile.read(), height=660, scrolling=True)
 with col2:
-    for i in legend_list:
+    # show the legend of graph
+    # extract the lengends from legend_list 
+    # legend_list is like: [[("", "", colour0), class0], [("", "", colour1), class1]......]
+    for i in legend_list: 
             annotated_text(i)
 
 components.html(HtmlFile.read(),
